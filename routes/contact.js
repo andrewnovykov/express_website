b4911feea270d96e4c70af02844dfe2befc8098a
papapin <papapin777@gmail.com>
2016-02-27 19:50:13 +0200

    step#3

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
 });
+
+
+router.post('/send',function(req, res, next){
+	var transporter = nodemailer.createTransport({
+		servise: 'Gmail',
+		auth: {
+			user: 'john@gmail.com',
+			pass: 'password'
+		}
+
+	});
+
+	var mailOptions = {
+		from: 'John Doe <john@gmail.com>',
+		to: 'admin@studio.com.ua',
+		subject: 'Website submission',
+		text: 'Letter .... Name: '+req.body.name+' Email: '+req.body.email+' Message: '+req.body.message,
+		html: '<p>  HIs Name: '+req.body.name+' </p>'
+
+
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+			res.redirect('/');
+		} else {
+			console.log('Message sent'+info.resopnse);
+			res.redirect('/contact');
+		}
+	});
+
+} );
+
 
 module.exports = router;
